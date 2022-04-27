f4f5c2e89c4057bb1746b5014a27308acc29dedb
Tyler Twisle <102629991+TwisleOffical@users.noreply.github.com>
2022-04-26 23:35:29 -0400

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,67 @@
+Citizen.CreateThread(function()
+        if true then
+            if Config.Balla.Ballas then
+        Citizen.Wait(1)
+    local BallaPos = vector3(114.690, -1947.890, 20.000)
+        Citizen.Wait(1)
+        local playerped = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerped)
+        local distanceBalla = #(playerCoords - BallaPos)
+        local IsInAreaBalla = false 
+
+            if distanceBalla < 20.0 then
+                IsInAreaBalla = true
+            else
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_BALLAS'), GetHashKey('PLAYER'))
+                SetRelationshipBetweenGroups(1, GetHashKey('AMBIENT_GANG_BALLAS'), GetHashKey('AMBIENT_GANG_BALLAS'))
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_BALLAS'), GetHashKey('AMBIENT_GANG_MEXICAN'))
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_FAMILY'))
+     end
+    end
+end
+end)
+
+Citizen.CreateThread(function()
+        if true then
+            if Config.Vago.Vagos then
+        Citizen.Wait(1)
+    local VagosPos = vector3(346.470, -2050.030, 21.000)
+        Citizen.Wait(1)
+        local playerped = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerped)
+        local distanceVagos = #(playerCoords - VagosPos)
+        local IsInAreaVagos = false 
+
+            if distanceVagos < 20.0 then
+                IsInAreaVagos = true
+            else
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('PLAYER'))
+                SetRelationshipBetweenGroups(1, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_MEXICAN'))
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_BALLAS'))
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_BALLAS'))
+     end
+    end
+end
+end)
+
+Citizen.CreateThread(function()
+    local FamilesPos = vector3(-117.46, -1526.66, 33.000)
+        if true then
+            if Config.Famile.Familes then
+        Citizen.Wait(1)
+        local playerped = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerped)
+        local distanceFamiles = #(playerCoords - FamilesPos)
+        local IsInAreaFamiles = false 
+
+            if distanceFamiles < 20.0 then
+                IsInAreaFamiles = true
+            else
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('PLAYER'))
+                SetRelationshipBetweenGroups(1, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('AMBIENT_GANG_FAMILY'))
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('AMBIENT_GANG_BALLAS'))
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('AMBIENT_GANG_MEXICAN'))
+     end
+    end
+end
+end)
---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Balla = {
+    Ballas = true
+}
+
+Config.Vago = {
+    Vagos = true
+}
+
+Config.Famile = {
+    Familes = true
+}
