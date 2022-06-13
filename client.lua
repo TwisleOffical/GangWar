4e89392932fa9acccd0b1d4ce7a6255569e59264
Tyler Twisle <102629991+TwisleOffical@users.noreply.github.com>
2022-06-13 01:55:15 -0400

    Big Update
    Fixed It So They Only Shoot When You Are In The Area
    Delete Some Un Needed Code
    Added The Lost MC

--- client.lua
+++ client.lua
@@ -1,21 +1,19 @@
 Citizen.CreateThread(function()
         if true then
-            if Config.Balla.Ballas then
+            if Config.Balla.Ballas == true then
         Citizen.Wait(1)
     local BallaPos = vector3(114.690, -1947.890, 20.000)
         Citizen.Wait(1)
         local playerped = PlayerPedId()
         local playerCoords = GetEntityCoords(playerped)
-        local distanceBalla = #(playerCoords - BallaPos)
         local IsInAreaBalla = false 
 
-            if distanceBalla < 20.0 then
+            if (GetDistanceBetweenCoords(BallaPos.x, BallaPos.y, BallaPos.z, GetEntityCoords(ped))) < 10 then
                 IsInAreaBalla = true
             else
                 SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_BALLAS'), GetHashKey('PLAYER'))
                 SetRelationshipBetweenGroups(1, GetHashKey('AMBIENT_GANG_BALLAS'), GetHashKey('AMBIENT_GANG_BALLAS'))
-                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_BALLAS'), GetHashKey('AMBIENT_GANG_MEXICAN'))
-                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_FAMILY'))
+                          
      end
     end
 end
@@ -23,44 +21,57 @@ end)
 
 Citizen.CreateThread(function()
         if true then
-            if Config.Vago.Vagos then
+            if Config.Vago.Vagos == true then
         Citizen.Wait(1)
     local VagosPos = vector3(346.470, -2050.030, 21.000)
         Citizen.Wait(1)
         local playerped = PlayerPedId()
         local playerCoords = GetEntityCoords(playerped)
-        local distanceVagos = #(playerCoords - VagosPos)
         local IsInAreaVagos = false 
 
-            if distanceVagos < 20.0 then
+            if (GetDistanceBetweenCoords(VagosPos.x, VagosPos.y, VagosPos.z, GetEntityCoords(ped))) < 20 then
                 IsInAreaVagos = true
             else
                 SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('PLAYER'))
                 SetRelationshipBetweenGroups(1, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_MEXICAN'))
-                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_BALLAS'))
-                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_BALLAS'))
      end
     end
 end
 end)
 
 Citizen.CreateThread(function()
-    local FamiliesPos = vector3(-117.46, -1526.66, 33.000)
+    local FamilesPos = vector3(-117.46, -1526.66, 33.000)
         if true then
-            if Config.Familie.Families then
+            if Config.Famile.Familes == true then
         Citizen.Wait(1)
         local playerped = PlayerPedId()
         local playerCoords = GetEntityCoords(playerped)
-        local distanceFamilies = #(playerCoords - FamiliesPos)
-        local IsInAreaFamilies = false 
+        local IsInAreaFamiles = false 
 
-            if distanceFamilies < 20.0 then
-                IsInAreaFamilies = true
+            if (GetDistanceBetweenCoords(FamilesPos.x, FamilesPos.y, FamilesPos.z, GetEntityCoords(ped))) < 20 then
+                IsInAreaFamiles = true
             else
                 SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('PLAYER'))
                 SetRelationshipBetweenGroups(1, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('AMBIENT_GANG_FAMILY'))
-                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('AMBIENT_GANG_BALLAS'))
-                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('AMBIENT_GANG_MEXICAN'))
+     end
+    end
+end
+end)
+
+Citizen.CreateThread(function()
+    local LostPos = vector3(61.05, 3709.82, 39)
+        if true then
+            if Config.Lost.LostMC == true then
+        Citizen.Wait(1)
+        local playerped = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerped)
+        local IsInAreaLost = false 
+
+            if (GetDistanceBetweenCoords(LostPos.x, LostPos.y, LostPos.z, GetEntityCoords(ped))) < 10 then
+                IsInAreaLost = true
+            else
+                SetRelationshipBetweenGroups(5, GetHashKey('AMBIENT_GANG_LOST'), GetHashKey('PLAYER'))
+                SetRelationshipBetweenGroups(1, GetHashKey('AMBIENT_GANG_LOST'), GetHashKey('AMBIENT_GANG_LOST'))
      end
     end
 end
